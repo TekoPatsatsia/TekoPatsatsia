7fe265ce32fde0ea00266b461f8c693db446341a
TekoPatsatsia <Tekopatsatsia@gmail.com>
2021-12-17 18:42:56 +0400

    task8

--- TekoPatsatsia/Program.cs
+++ TekoPatsatsia/Program.cs
@@ -6,3 +6,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(Task6.Insert(5));
 Console.WriteLine(Task6.Insert(7));
 Console.WriteLine(Task6.Remove(5));
+
+Task8.CreateDict();
+Console.WriteLine(Task8.ExchangeRate("USD", "EUR"));
--- TekoPatsatsia/Tasks/Task8.cs
+++ TekoPatsatsia/Tasks/Task8.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TekoPatsatsia.Tasks
 {
     public class Task8
     {
+        private static readonly Dictionary<string, double> Rates = new();
+        public static void CreateDict()
+        {
+            var xmlString = "";
+            const string pattern = @"<td>(.*?)<\/td>";
+            var rgx = new Regex(pattern);
+            using (var request = new WebClient())
+            {
+                var requestData = request.DownloadData("http://www.nbg.ge/rss.php");
 
+                xmlString = Encoding.ASCII.GetString(requestData);
+            }
+            var i = 0;
+            var key = "";
+            foreach (Match match in rgx.Matches(xmlString))
+            {
+                var val = match.Value.Substring(4, match.Value.Length - 9);
+                if (i % 5 == 2)
+                {
+                    Rates[key] = Convert.ToDouble(val);
+                }
+                if (i % 5 == 0)
+                {
+                    key = val;
+                }
+                i++;
+            }
+        }
+
+        public static double ExchangeRate(string from, string to)
+        {
+            return Rates[from] / Rates[to];
+        }
     }
 }
